d5b36d88f80655d67fd25a0455b4df9512370c6d
Maxwell Oldshein <maxwell.oldshein@gmail.com>
2026-02-09 18:57:55 -0500

    Structure scrubber.py similar to exporter.py

--- utils/scrubber.py
+++ utils/scrubber.py
@@ -3,8 +3,11 @@ import praw
 import time
 
 def scrub_content(reddit: praw.Reddit):
+    user_posts = reddit.user.me().submissions.new(limit=1000)
+    user_comments = reddit.user.me().comments.new(limit=1000)
+        
     print("\nStarting retrieval, editing and then deleting user submitted posts...\n")
-    for post in reddit.user.me().submissions.new(limit=1000):
+    for post in user_posts:
         if post.is_self:
             # Update the text of the post to conceal its original contents prior to deletion.
             post.edit(f"[deleted] {datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')}")
@@ -18,7 +21,7 @@ def scrub_content(reddit: praw.Reddit):
     
     print("\nStarting editing and then deleting user submitted comments...\n")
 
-    for comment in reddit.user.me().comments.new(limit=1000):
+    for comment in user_comments:
         # Update the text of the post to conceal its original contents prior to deletion.
         comment.edit("[deleted] " + datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S"))
         time.sleep(1)
